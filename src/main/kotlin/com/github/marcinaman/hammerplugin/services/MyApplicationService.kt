229314315663e05bd8143600e6a685498dbe06cb
GitHub Action <action@github.com>
2023-01-02 20:04:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/marcinaman/hammerplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/marcinaman/hammerplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.marcinaman.hammerplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marcinaman.hammerplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/marcinaman/hammerplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/marcinaman/hammerplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.marcinaman.hammerplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.marcinaman.hammerplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
